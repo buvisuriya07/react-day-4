0703cfb096208aab7b12f8bd0bbcd4bbd9f3d273
buvisuriya07 <70657998+buvisuriya07@users.noreply.github.com>
2023-09-24 04:46:49 +0530

    Create MenuBar.jsx

---
+++ src/Components/MenuBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import SpeedIcon from "@mui/icons-material/Speed";
+
+
+const MenuBar = () => {
+  return (
+    <div className="sidebar">
+      <h2>Admin</h2>
+      <h4>
+        <p>
+          <SpeedIcon />
+        </p>
+        DashBoard
+      </h4>
+    </div>
+  );
+};
+
+export default MenuBar;
